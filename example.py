ca92447b32dfd7595e4d08fc97e4e6cbd0f03393
user6174 <enne100724@gmail.com>
2020-06-28 19:00:56 +0200

    Add files via upload

--- example.py
+++ example.py
@@ -1,6 +1,15 @@
 import json
-with open('Neutral.json', 'r') as f:
-    cardpool = json.load(f)
-    card = cardpool["Robogoblin"]
-    for key in card:
-        print(f'{key:<12} -> {card[key]}')
+import os
+# load the entire card library
+cardpool = {}
+for subdir, _, files in os.walk(os.getcwd()):
+    for f in files:
+        if str(f).endswith(".json"):
+            with open(os.path.join(subdir, f), 'r') as jsonf:
+                tmp = json.load(jsonf)
+                for card in tmp:
+                    cardpool[card] = tmp[card]
+# usage example
+card = cardpool["Robogoblin"]
+for key in card:
+    print(f'{key:<12} -> {card[key]}')
