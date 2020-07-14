889e3cf225e506c3deac7addbf4715677e8c5870
user6174 <enne100724@gmail.com>
2020-07-14 23:27:15 +0200

    added alts_ tag

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import os
 with open(f'{os.getcwd()}/Neutral.json', 'r') as f:
         cardpool = json.load(f)
 
-card = cardpool["Robogoblin"]
+card = cardpool["Desert Pathfinder"]
 for key in card:
     type_ = str(type(card[key]))[8:-2]
     print(f'{key:<13} -> ({type_}) {card[key]}')
