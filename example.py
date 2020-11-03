e749135584640a2bd9073c2adbf7f1d6a372b3cb
user6174 <enne100724@gmail.com>
2020-11-03 15:15:34 +0100

    updated to oct 28th nerfs, fixed "treated as" not in alts_

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
 import json
 import os
+
 with open(f'{os.getcwd()}/en/all.json', 'r') as f:
-        cardpool = json.load(f)
+    cardpool = json.load(f)
 
 for k, v in cardpool['114031010'].items():
     print(f'{k:<13} -> ({str(type(v))[8:-2]}) {v}')
